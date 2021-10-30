6cb2ae1a255e5fd14a083b0b254bc5fc862e2432
Davide Testoni <davide@testoni.net>
2021-10-31 01:09:10 +0200

    Fix 500 on login with blank username

--- API/DTOs/LoginDto.cs
+++ API/DTOs/LoginDto.cs
@@ -1,10 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace API.DTOs
+﻿namespace API.DTOs
 {
     public class LoginDto
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -5,10 +5,10 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        public string UserName { get; set; }
+        public string UserName { get; set; } = string.Empty;
 
         [Required]
         [StringLength(32, MinimumLength = 8)]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
     }
 }
