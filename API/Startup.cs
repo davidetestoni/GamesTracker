ad04b67864e7f6a3c49e4fae806e7f0f7c81bad3
Davide Testoni <davide@testoni.net>
2021-11-05 17:14:51 +0100

    Change to not serve static files in dev

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,20 +1,14 @@
 using System;
-using System.Text;
 using API.Controllers;
-using API.Data;
 using API.Extensions;
 using API.Helpers;
 using API.Interfaces;
 using API.Middlewares;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -52,14 +46,12 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-            */
 
             app.UseMiddleware<ExceptionMiddleware>();
 
@@ -73,14 +65,21 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
-            // Serves index.html from wwwroot
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                // Serves index.html from wwwroot
+                app.UseDefaultFiles();
+                app.UseStaticFiles();
+            }
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapFallbackToController(nameof(FallbackController.Index), "Fallback");
+
+                if (!env.IsDevelopment())
+                {
+                    endpoints.MapFallbackToController(nameof(FallbackController.Index), "Fallback");
+                }
             });
         }
     }
